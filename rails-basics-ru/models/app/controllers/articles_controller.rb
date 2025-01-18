f076516f80635769be7589ff2d5f62c963605494
@hexlet/cli <support@hexlet.io>
2025-01-18 21:23:12 +0300

    submit rails-basics-ru/models

---
+++ rails-basics-ru/models/app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+end
